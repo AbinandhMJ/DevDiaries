4f53f0b375e146da3265992d5d882dc6ccf8c40d
AbinandhMJ <abinandhmurukesan@gmail.com>
2024-01-28 23:10:05 +0530

    Model additional features added

--- blog/blogger/models.py
+++ blog/blogger/models.py
@@ -1,6 +1,8 @@
+import requests
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
@@ -11,7 +13,6 @@ class Comment(models.Model):
     def __str__(self):
         return f"{self.author} on '{self.post}'"
 
-
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
@@ -20,7 +21,6 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-
 
 class Post(models.Model):
     options = (
@@ -33,13 +33,41 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_author")
     content = models.TextField()
+    image = models.ImageField(upload_to="post_images/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     last_updated = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField("Category", related_name="posts")
     status = models.CharField(max_length=10, choices=options, default="draft")
+    views = models.PositiveIntegerField(default=0)
 
     class Meta:
-        ordering = ("-created_at",) 
+        ordering = ("-created_at",)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        # If no image is provided, fetch an image from Pexels API
+        if not self.image:
+            self.fetch_image_from_pexels()
+        super().save(*args, **kwargs)
+
+    def fetch_image_from_pexels(self):
+        # Fetch image from Pexels API based on the post title
+        query = slugify(self.title)
+        headers = {"Authorization": "cfqed7d3TCjPaX70zHILKTtFad2T8or7lkEmx2A1NtxtylDrIYgt33AV"}
+        params = {"query": query, "per_page": 1}
+        response = requests.get("https://api.pexels.com/v1/search", headers=headers, params=params)
+
+        if response.status_code == 200:
+            image_url = self.extract_image_url(response.json())
+            if image_url:
+                response = requests.get(image_url)
+                self.image.save(f"{slugify(self.title)}.jpg", ContentFile(response.content), save=False)
+
+    def extract_image_url(self, response_json):
+        # Extract the first image URL from the Pexels API response
+        photos = response_json.get("photos", [])
+        if photos:
+            return photos[0].get("src", {}).get("large", None)
+        return None
--- blog/blogger/views.py
+++ blog/blogger/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post, Category, Comment
 
 class HomeView(ListView):
     model = Post
-    model = Category
-    model = Comment
-
     template_name = "blog/index.html"
+    context_object_name = "posts"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['comments'] = Comment.objects.all()
+        return context
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/single-post.html" 
+    context_object_name = "post"
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset=queryset)
+        obj.views += 1
+        obj.save()
+        return obj
